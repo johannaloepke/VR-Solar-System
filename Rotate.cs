6aef2f8c1b0440b49b5b8e046e22113b025c0086
johannaloepke <johanna.loepke@gmail.com>
2017-01-18 11:06:45 -0500

    Added original files

---
+++ Rotate.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    public GameObject obj;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.RotateAround(obj.transform.position, Vector3.up, 20 * Time.deltaTime);
+    }
+}
